7ec149d8cb1ea03eead417bdd30be1d534ac7989
abhipsaadya <129159833+abhipsaadya@users.noreply.github.com>
2023-03-28 23:22:40 +0530

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-import { fontSize } from '@mui/system';
+
 const Footer = () => {
   return (
     <>
